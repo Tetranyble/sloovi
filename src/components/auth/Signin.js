8e6727bc69f109d08cc58f090ada00526dbaece7
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2022-06-28 15:52:04 +0100

    update signin page

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -22,11 +22,11 @@ class Signin extends Component {
           
           <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-          <div className="form-floating">
+          <div className="form-floating form-group">
             <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
             <label htmlFor="floatingInput">Email address</label>
           </div>
-          <div className="form-floating">
+          <div className="form-floating form-group">
             <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
             <label htmlFor="floatingPassword">Password</label>
           </div>
