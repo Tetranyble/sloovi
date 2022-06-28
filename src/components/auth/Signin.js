13881e26e06b2a85e34011eb1369ee66ee4002e1
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2022-06-28 15:46:33 +0100

    change sign template

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -4,6 +4,8 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+import './SigninStyle.css';
+
 class Signin extends Component {
   onSubmit = formProps => {
     this.props.signin(formProps, () => {
@@ -13,31 +15,57 @@ class Signin extends Component {
 
   render() {
     const { handleSubmit } = this.props;
-
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <fieldset>
-          <label>Email</label>
-          <Field
-            name="email"
-            type="text"
-            component="input"
-            autoComplete="none"
-          />
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-          <Field
-            name="password"
-            type="password"
-            component="input"
-            autoComplete="none"
-          />
-        </fieldset>
-        <div>{this.props.errorMessage}</div>
-        <button>Sign In!</button>
-      </form>
+      <div className='container'>
+        <main className="form-signin w-100 m-auto">
+        <form>
+          
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+
+          <div className="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+        </form>
+      </main>
+      </div>
     );
+    // return (
+    //   <form onSubmit={handleSubmit(this.onSubmit)}>
+    //     <fieldset>
+    //       <label>Email</label>
+    //       <Field
+    //         name="email"
+    //         type="text"
+    //         component="input"
+    //         autoComplete="none"
+    //       />
+    //     </fieldset>
+    //     <fieldset>
+    //       <label>Password</label>
+    //       <Field
+    //         name="password"
+    //         type="password"
+    //         component="input"
+    //         autoComplete="none"
+    //       />
+    //     </fieldset>
+    //     <div>{this.props.errorMessage}</div>
+    //     <button>Sign In!</button>
+    //   </form>
+    // );
   }
 }
 
