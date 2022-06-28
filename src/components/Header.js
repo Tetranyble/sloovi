10c0f69a0709897c3a3dac1116afb8af05f9a84f
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2022-06-28 15:28:34 +0100

    update navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,11 +22,33 @@ class Header extends Component {
     }
   }
 
+  renderHeader = () =>{
+    return (
+      <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <Link href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+          Sloovi
+        </Link>
+  
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><Link to="/" className="nav-link px-2 link-secondary">Home</Link></li>
+          <li><Link to="/feature" className="nav-link px-2 link-dark">Features</Link></li>
+        </ul>
+  
+        <div className="col-md-3 text-end">
+          <Link to="/signin" className="btn btn-outline-primary me-2">Login</Link>
+          <Link to="/signup" className="btn btn-primary">Sign-up</Link>
+        </div>
+      </header>
+    </div>
+    );
+  }
+
   render() {
     return (
       <div className="header">
         <Link to="/">Redux Auth</Link>
-        {this.renderLinks()}
+        {this.renderHeader()}
       </div>
     );
   }
