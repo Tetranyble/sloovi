8ee771beb4681187d3b5fe67ecc0cd9a8cc63115
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2022-06-28 15:58:22 +0100

    update input to use redux-form

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -18,54 +18,32 @@ class Signin extends Component {
     return (
       <div className='container'>
         <main className="form-signin w-100 m-auto">
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
           
           <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
           <div className="form-floating form-group">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <Field type="email" component="input" name="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
             <label htmlFor="floatingInput">Email address</label>
           </div>
           <div className="form-floating form-group">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <Field type="password" component="input" name="password" className="form-control" id="floatingPassword" placeholder="Password" />
             <label htmlFor="floatingPassword">Password</label>
           </div>
 
           <div className="checkbox mb-3">
             <label>
-              <input type="checkbox" value="remember-me" /> Remember me
+              <Field type="checkbox" component="input" name="remember" value="remember" /> Remember me
             </label>
           </div>
           <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+          <p className="mt-5 mb-3 text-muted">&copy; 2022</p>
         </form>
       </main>
       </div>
     );
-    // return (
-    //   <form onSubmit={handleSubmit(this.onSubmit)}>
-    //     <fieldset>
-    //       <label>Email</label>
-    //       <Field
-    //         name="email"
-    //         type="text"
-    //         component="input"
-    //         autoComplete="none"
-    //       />
-    //     </fieldset>
-    //     <fieldset>
-    //       <label>Password</label>
-    //       <Field
-    //         name="password"
-    //         type="password"
-    //         component="input"
-    //         autoComplete="none"
-    //       />
-    //     </fieldset>
-    //     <div>{this.props.errorMessage}</div>
-    //     <button>Sign In!</button>
-    //   </form>
-    // );
+   
+    
   }
 }
 
